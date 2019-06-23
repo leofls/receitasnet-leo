4280d7de9cd4c30f6c9d0700ca966dba36d8c673
Leandro Ferreira <leo.f.l.s@hotmail.com>
2019-06-23 17:41:21 -0300

    Criando helpers

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,10 @@
 module RecipesHelper
+
+    def formata_caloria(recipe)
+        if recipe.light?
+            content_tag(:trong, "Light (Menos de 100 Calorias)")
+        else
+            recipe.calories
+        end
+    end
 end
