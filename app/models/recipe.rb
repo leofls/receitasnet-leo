6054744ce9c74de8eb7629d558d4fb8930d5b690
Leandro Ferreira <leo.f.l.s@hotmail.com>
2019-07-04 09:39:43 -0300

    Criando filtros customizados

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,10 @@
 class Recipe < ApplicationRecord
 
     def light?
-        calories < 100
+        calories.blank? || calories < 100
+    end
+
+    def self.created_at
+        where('created_at <= ?', Time.now).order('created_at desc')
     end
 end
